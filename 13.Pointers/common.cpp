5d2e793c5316120c6c3c5d7e47c3f17c1c5bba5c
adityasharma736 <adityasharma736630@gmail.com>
2024-09-05 15:52:39 +0530

    Pointers

---
+++ 13.Pointers/common.cpp
@@ -0,0 +1,96 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 10;
+//     int* ptr = &a;
+//     cout<<sizeof(ptr)<<endl;
+//     char ch = 'a';
+//     char* cha = &ch;
+//     cout<<sizeof(cha)<<endl;
+//     double d = 12.09;
+//     double* dptr = &d;
+//     cout<<sizeof(dptr)<<endl;
+// }
+#include<iostream>
+#include<string>
+using namespace std;
+// void funtionSize(int array[]){
+//     cout<<"Size of array is "<<sizeof(array)<<endl;
+// }
+int main(){
+    // int array[4]={1,2,3,4};
+    // int* p = array;
+    // int* ptr = &array[10];
+    // cout<<sizeof(ptr)<<endl;
+    // cout<<ptr[1]<<endl;
+    // cout<<ptr[0]<<endl;
+    // cout<<
+    // cout<<&array[0]<<endl;
+    // cout<<&array<<endl;
+    // cout<<array<<endl;
+    // cout<<array[0]<<endl;
+    // cout<<*p<<endl;
+    // cout<<0[array]<<endl;
+    // cout<<array[1]<<endl;
+    // cout<<*(p+1)<<endl;
+    // cout<<1[array]<<endl;
+    // cout<<array[2]<<endl;
+    // cout<<*(p+2)<<endl;
+    // cout<<2[array]<<endl;
+    // cout<<array[3]<<endl;
+    // cout<<*(p+3)<<endl;
+    // cout<<3[array]<<endl;
+
+    // cout<<sizeof(array)<<endl;
+    // cout<<sizeof(p)<<endl;
+    // cout<<sizeof(*p)<<endl;
+
+    // char ch[10] ="aditya";
+    // char* ptr = ch;
+    // cout<<ch<<endl;
+    // cout<<*(ch+2)<<endl;
+    // cout<<ptr<<endl;
+    // cout<<*(ptr+1)<<endl;
+    // cout<<ptr+2<<endl;
+    // char ch = 'b';
+    // char* ptr = &ch;
+    // cout<<ptr<<endl;
+
+    // int array[10] = {1,2,3,4,5};
+    // cout<<"size of array "<<sizeof(array)<<endl;
+    // funtionSize(array);
+    // cout<<"size of array"<<size<<endl;
+
+
+// float f = 10.5;
+// float p = 2.5;
+// float* ptr = &f;
+// (*ptr)++;
+// *ptr=p;
+// cout<<*ptr<<" "<<f<<" "<<p;
+    
+    // int a = 7;
+    // int b = 17;
+    // int* c = &b;
+    // *c = 7;
+    // cout<<a<<" "<<b;
+    // int* ptr = 0;
+    // int a = 8;
+    // *ptr = a;
+    // cout<<*ptr<<endl;
+    // int a = 7;
+    // int *c = &a;
+    // c=c+3;
+    // cout<<c<<endl;
+    // double a = 10.54;
+    // double*b = &a;
+    // b= b+1;
+    // cout<<&a<<endl;
+    // cout<<b<<endl;
+    // int array[]= {1,2,3,4};
+    // int* p = (array+1);
+    // cout<<*array +9;
+    
+
+    
+}
