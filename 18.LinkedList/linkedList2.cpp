36c397404892b906eb910cc38e3a303c62febeb5
adityasharma736 <adityasharma736630@gmail.com>
2024-11-18 08:43:05 +0530

    linkdedList pratice

--- 18.LinkedList/linkedList2.cpp
+++ 18.LinkedList/linkedList2.cpp
@@ -39,12 +39,57 @@ void revereNode(Node*tail){
         temp = temp->prev;
     }
 }
+void insertAtTail(int value,Node* & head,Node*& tail){
+    if(head==NULL && tail == NULL){
+        Node* newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }else{
+        Node* newNode = new Node(value);
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+}
+int getLength(Node*& head){
+    Node* temp = head;
+    int count = 0;
+    while(temp!=NULL){
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+void insertAtPosition(int place,int value,Node*&head,Node*& tail){
+    int lengh= getLength(head);
+    if(place == 1){
+        insertAtHead(value,head,tail);
+    }else if(place == lengh+1){
+        insertAtTail(value,head,tail);
+    }else{
+        Node* backWard = head;
+        for(int i = 0;i<lengh-2;i++){
+            backWard= backWard->next;
+        }
+        Node* forward = backWard->next;
+        Node* newNode = new Node(value);
+        newNode->prev = backWard;
+        backWard->next = newNode;
+        newNode->next = forward;
+        forward->prev = newNode;
+    }
+}
 int main(){
     Node* head = NULL;
     Node* tail = NULL;
-    insertAtHead(10,head,tail);
-    insertAtHead(20,head,tail);
+    // insertAtHead(10,head,tail);
+    // insertAtHead(20,head,tail);
+    // print(head);
+    // revereNode(tail);
+    insertAtTail(10,head,tail);
+    insertAtTail(20,head,tail);
     print(head);
-    revereNode(tail);
+    insertAtPosition(2,30,head,tail);
+    print(head);
     return 0;
 };
